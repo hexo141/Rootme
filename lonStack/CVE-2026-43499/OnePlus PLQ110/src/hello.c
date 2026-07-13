923b7f00edb9ae0f2fa6810102515b7fe42f8b8b
wnwn <1337075411@qq.com>
2026-07-13 15:32:46 +0800

    进行大换血

--- lonStack/CVE-2026-43499/OnePlus PLQ110/src/hello.c
+++
@@ -1,9 +0,0 @@
-// hello.c: NDK cross-compile sanity check for the OnePlus PLQ110 toolchain
-#include <stdio.h>
-int main(void) {
-    printf("[*] hello from OnePlus PLQ110 toolchain\n");
-    printf("[*] target: aarch64-linux-android\n");
-    printf("[*] sizeof(void*) = %zu\n", sizeof(void*));
-    printf("[*] sizeof(long)  = %zu\n", sizeof(long));
-    return 0;
-}
--- lonStack/CVE-2026-43499/OnePlus PLQ110/src/test_min.c
+++
@@ -1,6 +0,0 @@
-// test_min.c: smallest possible PIE binary to verify NDK toolchain works
-#include <unistd.h>
-int main(void) {
-    write(1, "[+] plq110 test_min ok\n", 23);
-    return 0;
-}
